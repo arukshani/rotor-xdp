014860775d75915662ec6b6352dad26994836c34
rukshani <rukshani@node-33.largecluster.rotornet-pg0.utah.cloudlab.us>
2024-03-25 16:33:38 -0600

    increase runtime

--- emulator-setup/start_opera.sh
+++ emulator-setup/start_opera.sh
@@ -4,5 +4,5 @@
 
 # sudo ./opera_nic 192.168.1.1 configs/node-1-link.csv 60
 cd /opt/rotor-xdp/emu-opera-final
-nohup sudo ./emu_nic $1 config/$2.csv /dev/$3 60 1 1 > /dev/null 2>&1 &
+nohup sudo ./emu_nic $1 config/$2.csv /dev/$3 120 1 1 > /dev/null 2>&1 &
 # nohup sudo ./opera_nic $1 configs/$2.csv $3 120 > /tmp/opera_nic.out 2>&1 &
