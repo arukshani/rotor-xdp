c18c19a22f5cd2f02c54283d743d47b4a5d0c1b0
rukshani <rukshani@node-33.largecluster.rotornet-pg0.utah.cloudlab.us>
2024-03-26 17:15:05 -0600

    analysis scripts

---
+++ z-analysis/collect_data.py
@@ -0,0 +1,43 @@
+import os
+import subprocess
+import ipaddress
+import pandas as pd
+import pickle
+import json 
+# import constant
+import binascii
+import socket
+import errno
+from datetime import datetime
+
+USER = os.environ['USER']
+IDENTITY_FILE = '/users/{}/.ssh/{}_cloudlab.pem'.format(USER, USER)
+
+def gather_data():
+    with open('/tmp/workers.pkl','rb') as f:  
+        workers = pickle.load(f)
+        mydir = os.path.join(
+            "/tmp/", 
+            datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
+        # print(mydir)
+        try:
+            os.makedirs(mydir)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise  # This was not a "directory exist" error..
+        for worker in workers:
+            remoteCmd = 'scp -o StrictHostKeyChecking=no {}:/tmp/opera_emu_data.csv {}'.format(worker['host'], mydir)
+            proc = subprocess.run(remoteCmd, shell=True)
+            new_filename = "afxdp-data-{}.csv".format(worker['host'])
+            cmd = "mv {}/opera_emu_data.csv {}/{}".format(mydir, mydir, new_filename)
+            subprocess.run(cmd, shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8').strip()
+
+def main():
+    # check if identity file exists & works
+    if not os.path.isfile(IDENTITY_FILE):
+        print('Could not find identify file: {}. Please add it to this machine to run cloudlab setup'.format(IDENTITY_FILE))
+        exit(1)
+    
+if __name__ == '__main__':
+    main()
+    gather_data()
