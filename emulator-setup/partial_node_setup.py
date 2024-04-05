7fb126e2ebb7855ceab5c2e3764bd5c5afa5e954
rukshani <rukshani@node-33.largeclust.rotornet-pg0.utah.cloudlab.us>
2024-04-05 11:45:55 -0600

    add partial node setup

---
+++ emulator-setup/partial_node_setup.py
@@ -0,0 +1,93 @@
+import os
+import subprocess
+import ipaddress
+import pandas as pd
+import pickle
+import json 
+# import constant
+import binascii
+import socket
+
+USER = os.environ['USER']
+IDENTITY_FILE = '/users/{}/.ssh/{}_cloudlab.pem'.format(USER, USER)
+DATAPATH_INTERFACE="enp65s0f0np0"
+
+###################################################
+
+def copy_worker_info():
+    with open('/tmp/workers.pkl','rb') as f:  
+        workers = pickle.load(f)
+        for worker in workers:
+            if (worker['host'] == "node-1"):
+                remoteCmd = 'scp -o StrictHostKeyChecking=no /tmp/all_worker_info.csv {}:/tmp'.format(worker['host'])
+                proc = subprocess.run(remoteCmd, shell=True)
+
+def write_veth_info():
+     with open('/tmp/workers.pkl','rb') as f:  
+        workers = pickle.load(f)
+        for worker in workers:
+            if (worker['host'] == "node-1"):
+                remoteCmd = 'ssh -o StrictHostKeyChecking=no {}@{} "bash -s" < ./get_veth_info.sh {}'.format(worker['username'], worker['host'], worker['host'])
+                proc = subprocess.run(remoteCmd, shell=True)
+
+def get_worker_mac():
+    with open('/tmp/workers.pkl','rb') as f:  
+        workers = pickle.load(f)
+        for worker in workers:
+            if (worker['host'] == "node-1"):
+                ##First delete the specific row
+                with open("/tmp/all_worker_info.csv", "r") as f:
+                    lines = f.readlines()
+                with open("/tmp/all_worker_info.csv", "w") as f:
+                    x = 1
+                    for line in lines:
+                        if (x != 1):
+                            f.write(line)
+                        x = x + 1
+
+                remoteCmd = 'ssh -o StrictHostKeyChecking=no {}@{} "bash -s" < ./get_mac.sh {}'.format(worker['username'], worker['host'], worker['ip_lan'])
+                stdout = subprocess.run(remoteCmd, shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8').strip()
+                hex_val="0x{}".format(binascii.hexlify(socket.inet_aton(worker['ip_lan'])).decode('ascii'))
+                stdout = stdout + ",{},{},{}".format(worker['username'], worker['host'], hex_val)
+                with open("/tmp/all_worker_info.csv", "a") as myfile:
+                    myfile.write(stdout + "\n")
+
+def change_iface_properties():
+    with open('/tmp/workers.pkl','rb') as f:  
+        workers = pickle.load(f)
+        for worker in workers:
+            if (worker['host'] == "node-1"):
+                remoteCmd = 'ssh -o StrictHostKeyChecking=no {}@{} "bash -s" < ./change_iface_properties.sh {}'.format(worker['username'], worker['host'], worker['ip_lan'])
+                proc = subprocess.run(remoteCmd, shell=True)
+
+def create_containers():
+    with open('/tmp/workers.pkl','rb') as f:  
+        workers = pickle.load(f)
+        for worker in workers:
+            if (worker['host'] == "node-1"):
+                remoteCmd = 'ssh -o StrictHostKeyChecking=no {}@{} "bash -s" < ./create_containers.sh {}'.format(worker['username'], worker['host'], worker['ip_lan'])
+                proc = subprocess.run(remoteCmd, shell=True)
+
+def setup_workers():
+    with open('/tmp/workers.pkl','rb') as f:  
+        workers = pickle.load(f)
+        for worker in workers:
+            if (worker['host'] == "node-1"):
+                remoteCmd = 'ssh -o StrictHostKeyChecking=no {}@{} "bash -s" < ./setup_worker.sh {}'.format(worker['username'], worker['host'], worker['ip_lan'])
+                proc = subprocess.run(remoteCmd, shell=True)
+
+def main():
+    # check if identity file exists & works
+    if not os.path.isfile(IDENTITY_FILE):
+        print('Could not find identify file: {}. Please add it to this machine to run cloudlab setup'.format(IDENTITY_FILE))
+        exit(1)
+    
+if __name__ == '__main__':
+    main()
+    setup_workers()
+    create_containers()
+    change_iface_properties()
+    get_worker_mac() ##In addition we first remove old record from /tmp/all_worker_info.csv
+    write_veth_info()
+    copy_worker_info()
+    
