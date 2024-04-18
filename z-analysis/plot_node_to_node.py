1c1a2c38a52c7023e0047e832284dfb09d14c285
rukshani <rukshani@node-33.operacluster.rotornet-pg0.utah.cloudlab.us>
2024-04-18 15:46:37 -0600

    plot

--- z-analysis/plot_node_to_node.py
+++ z-analysis/plot_node_to_node.py
@@ -41,8 +41,8 @@ udp_rtt['rtt_us'] = udp_rtt.apply(lambda row: get_rtt(row['recv_time_part_sec'],
 
 ######################################AF_XDP DELAY######################################
 
-node_1_file = "afxdp_node1.csv"
-node_2_file = "afxdp_node2.csv"
+node_1_file = "2024-04-18_15-18-56/afxdp-data-node-1.csv"
+node_2_file = "2024-04-18_15-18-56/afxdp-data-node-2.csv"
 ## node_ip,slot,topo_arr,next_node,time_ns,time_part_sec,time_part_nsec
 
 node_1 = pd.read_csv(path+node_1_file ,sep=',')
@@ -65,22 +65,24 @@ fwd_afxdp['fwd_delay_us'] = fwd_afxdp.apply(lambda row: get_rtt(row['recv_sec'],
 # print(n1_tx_df.head(5))
 # print(n2_rx_df.head(5))
 # print(fwd_afxdp.head(5))
-# n1_rx_df = node_1.loc[(node_1['slot'] == 2)]
-# n2_tx_df = node_2.loc[(node_2['slot'] == 0)]
-# n1_rx_df.reset_index(inplace=True)
-# n2_tx_df.reset_index(inplace=True)
-# n2_tx_df = n2_tx_df.rename(columns={'slot': 'send_slot', 
-#                                     'time_part_sec': 'send_sec', 'time_part_nsec': 'send_nsec'})
-# n1_rx_df = n1_rx_df.rename(columns={'slot': 'recv_slot', 
-#                                     'time_part_sec': 'recv_sec', 'time_part_nsec': 'recv_nsec'})
-# return_afxdp = pd.concat([n2_tx_df, n1_rx_df], axis=1)
-# return_afxdp['return_delay_us'] = return_afxdp.apply(lambda row: get_rtt(row['recv_sec'], row['send_sec'],
-#                                                             row['recv_nsec'], row['send_nsec']), axis=1)
+n1_rx_df = node_1.loc[(node_1['slot'] == 2)]
+n2_tx_df = node_2.loc[(node_2['slot'] == 0)]
+n1_rx_df.reset_index(inplace=True)
+n2_tx_df.reset_index(inplace=True)
+n2_tx_df = n2_tx_df.rename(columns={'slot': 'send_slot', 
+                                    'time_part_sec': 'send_sec', 'time_part_nsec': 'send_nsec'})
+n1_rx_df = n1_rx_df.rename(columns={'slot': 'recv_slot', 
+                                    'time_part_sec': 'recv_sec', 'time_part_nsec': 'recv_nsec'})
+return_afxdp = pd.concat([n2_tx_df, n1_rx_df], axis=1)
+return_afxdp['return_delay_us'] = return_afxdp.apply(lambda row: get_rtt(row['recv_sec'], row['send_sec'],
+                                                            row['recv_nsec'], row['send_nsec']), axis=1)
 
-# fwd_afxdp.reset_index(inplace=True)
-# return_afxdp.reset_index(inplace=True)
-# af_xdp_delay_summary = pd.concat([fwd_afxdp['fwd_delay_us'], return_afxdp['return_delay_us']], axis=1)
-# af_xdp_delay_summary['total_delay_us'] = af_xdp_delay_summary.apply(lambda row: get_total_delay(row['fwd_delay_us'], row['return_delay_us']), axis=1)
+# print(return_afxdp[(return_afxdp['return_delay_us'] < 0)])
+
+fwd_afxdp.reset_index(inplace=True)
+return_afxdp.reset_index(inplace=True)
+af_xdp_delay_summary = pd.concat([fwd_afxdp['fwd_delay_us'], return_afxdp['return_delay_us']], axis=1)
+af_xdp_delay_summary['total_delay_us'] = af_xdp_delay_summary.apply(lambda row: get_total_delay(row['fwd_delay_us'], row['return_delay_us']), axis=1)
 # print(fwd_afxdp.head(5))
 # print(return_afxdp.head(5))
 # print(af_xdp_delay_summary.head(5))
@@ -93,6 +95,26 @@ fwd_afxdp['fwd_delay_us'] = fwd_afxdp.apply(lambda row: get_rtt(row['recv_sec'],
 # plt.legend()
 # plt.xlabel('Delay (us)')
 # plt.ylabel('CDF')
+# plt.savefig('af_xdp_one_way.png')
 
-# plt.savefig('udp_delay.png')
+hist, bin_edges = np.histogram(udp_rtt['rtt_us'], bins=100, density=True)
+cdf = np.cumsum(hist * np.diff(bin_edges))
+
+hist_1, bin_edges_1 = np.histogram(fwd_afxdp['fwd_delay_us'], bins=100, density=True)
+cdf_1 = np.cumsum(hist_1 * np.diff(bin_edges_1))
+
+hist_2, bin_edges_2 = np.histogram(return_afxdp['return_delay_us'], bins=100, density=True)
+cdf_2 = np.cumsum(hist_2 * np.diff(bin_edges_2))
+
+hist_3, bin_edges_3 = np.histogram(af_xdp_delay_summary['total_delay_us'], bins=100, density=True)
+cdf_3 = np.cumsum(hist_3 * np.diff(bin_edges_3))
+
+fig = go.Figure(data=[
+    go.Scatter(x=bin_edges, y=cdf, name='UDP RTT'),
+    go.Scatter(x=bin_edges_1, y=cdf_1, name='AFXDP FWD DELAY'),
+    go.Scatter(x=bin_edges_2, y=cdf_2, name='AFXDP RETURN DELAY'),
+    go.Scatter(x=bin_edges_3, y=cdf_3, name='AFXDP TOTAL DELAY'),
+])
 # fig = go.Figure()
+# fig.add_traces(go.Scatter(x=topo_results['packet_time'], y = topo_results['packets'], mode = 'lines', name=legend_str))
+fig.write_html("udp_results.html")
