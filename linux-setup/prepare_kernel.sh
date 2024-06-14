fd4d07ffad55766999c02c789a8b017ee3b39170
rukshani <rukshani@node-33.largecluster.rotornet-pg0.utah.cloudlab.us>
2024-06-14 05:22:59 -0600

    kernel setup

--- linux-setup/prepare_kernel.sh
+++ linux-setup/prepare_kernel.sh
@@ -10,19 +10,19 @@ export DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true
 sudo debconf-set-selections /opt/rotor-xdp/linux-setup/kexec-preseed.txt
 sudo apt-get install -y build-essential libncurses-dev bison flex libssl-dev libelf-dev kexec-tools python3 python3-pip
 
-pip install numpy pandas matplotlib cycler
+pip install -y numpy pandas matplotlib cycler
 
 echo "GRUB_DEFAULT=\"Advanced options for Ubuntu>Ubuntu, with Linux $(uname -r)\"" | sudo tee -a /etc/default/grub
 
 cp /opt/rotor-xdp/linux-setup/linux_config /opt/linux-5.15.1/
 mv /opt/linux-5.15.1/linux_config /opt/linux-5.15.1/.config
 
-sudo apt-get install libdw-dev
+sudo apt-get install -y libdw-dev
 cd /opt
 git clone https://github.com/acmel/dwarves.git
 cd /opt/dwarves/
 git checkout tags/v1.22
-sudo apt-get install cmake
+sudo apt-get install -y cmake
 sudo cmake -D__LIB=lib -DBUILD_SHARED_LIBS=OFF
 sudo make install
 
