04570f01644f648c6f251496f2f420a6602e555a
rukshani <rukshani@node-33.largecluster.rotornet-pg0.utah.cloudlab.us>
2024-06-17 01:41:56 -0600

    stats

--- emu-opera-encap-after/thread_functions_1.h
+++ emu-opera-encap-after/thread_functions_1.h
@@ -319,18 +319,18 @@ static void get_queue_index_for_nic_rx(void *data, struct port_params *params, u
 								sizeof(struct ethhdr) +
 								sizeof(struct iphdr));
 
-					seq[time_index] = ntohs(inner_tcp_hdr->seq);
+					seq[time_index] = ntohl(inner_tcp_hdr->seq);
 					//This field contains the upcoming sequence number and it acknowledges the feedback up to that.
 					// contains the value of the next sequence number that the sender of the segment is expecting to receive,
 					// if the ACK control bit is set. Note that the sequence number refers to the stream flowing in the same 
 					//direction as the segment, while the acknowledgement number refers to the stream flowing in the opposite 
 					//direction from the segment.
-					ack_seq[time_index] = ntohs(inner_tcp_hdr->ack_seq);
-					if (ntohs(inner_tcp_hdr->syn)) {
+					ack_seq[time_index] = ntohl(inner_tcp_hdr->ack_seq);
+					if (ntohl(inner_tcp_hdr->syn)) {
 						tcp_type[time_index] = 1;
-					} else if (ntohs(inner_tcp_hdr->ack)){
+					} else if (ntohl(inner_tcp_hdr->ack)){
 						tcp_type[time_index] = 2;
-					} else if (ntohs(inner_tcp_hdr->fin)){
+					} else if (ntohl(inner_tcp_hdr->fin)){
 						tcp_type[time_index] = 3;
 					} else {
 						tcp_type[time_index] = 0; // none of the above
@@ -373,18 +373,18 @@ static int encap_veth(int dest_index, void *data, struct port_params *params, ui
 					    sizeof(struct ethhdr) +
 					    sizeof(struct iphdr));
 
-			seq[time_index] = ntohs(inner_tcp_hdr->seq);
+			seq[time_index] = ntohl(inner_tcp_hdr->seq);
 			//This field contains the upcoming sequence number and it acknowledges the feedback up to that.
 			// contains the value of the next sequence number that the sender of the segment is expecting to receive,
 			// if the ACK control bit is set. Note that the sequence number refers to the stream flowing in the same 
 			//direction as the segment, while the acknowledgement number refers to the stream flowing in the opposite 
 			//direction from the segment.
-			ack_seq[time_index] = ntohs(inner_tcp_hdr->ack_seq);
-			if (ntohs(inner_tcp_hdr->syn)) {
+			ack_seq[time_index] = ntohl(inner_tcp_hdr->ack_seq);
+			if (ntohl(inner_tcp_hdr->syn)) {
 				tcp_type[time_index] = 1;
-			} else if (ntohs(inner_tcp_hdr->ack)){
+			} else if (ntohl(inner_tcp_hdr->ack)){
 				tcp_type[time_index] = 2;
-			} else if (ntohs(inner_tcp_hdr->fin)){
+			} else if (ntohl(inner_tcp_hdr->fin)){
 				tcp_type[time_index] = 3;
 			} else {
 				tcp_type[time_index] = 0; // none of the above
