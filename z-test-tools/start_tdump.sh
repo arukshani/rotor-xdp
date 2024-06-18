7ba8d8d841b901c930a5dba84b2bb88ab235277b
rukshani <rukshani@node-33.largecluster.rotornet-pg0.utah.cloudlab.us>
2024-06-17 21:22:42 -0600

    tdump

--- z-test-tools/start_tdump.sh
+++ z-test-tools/start_tdump.sh
@@ -3,9 +3,10 @@
 NODE_IN=$(ifconfig | grep -B1 "inet $1" | awk '$1!="inet" && $1!="--" {print $1}')
 NODE_IN=${NODE_IN::-1}
 
-pcap_name="host-br_time.pcap"
+pcap_name="host-tcp-dump.pcap"
 echo $pcap_name
 
 sudo rm -rf /tmp/$pcap_name
-sudo nohup tcpdump -i $NODE_IN -j adapter_unsynced -w /tmp/$pcap_name > /dev/null 2>&1 &
+# sudo nohup tcpdump -i $NODE_IN -j adapter_unsynced -w /tmp/$pcap_name > /dev/null 2>&1 &
+sudo nohup tcpdump -i $NODE_IN -w /tmp/$pcap_name > /dev/null 2>&1 &
 echo $! | sudo tee /var/run/tcpdump.pid
