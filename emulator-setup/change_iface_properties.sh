427b70359af093b3f44385c43a33e64714e90375
rukshani <rukshani@node-33.largecluster.rotornet-pg0.utah.cloudlab.us>
2024-06-21 05:52:02 -0600

    increase hw queue size

--- emulator-setup/change_iface_properties.sh
+++ emulator-setup/change_iface_properties.sh
@@ -5,3 +5,6 @@ NODE_INTERFACE=${NODE_INTERFACE::-1}
 
 sudo ip link set $NODE_INTERFACE mtu 3490
 sudo ethtool -L $NODE_INTERFACE combined 1
+
+sudo ethtool -G $NODE_INTERFACE rx 2048
+sudo ethtool -G $NODE_INTERFACE tx 2048
