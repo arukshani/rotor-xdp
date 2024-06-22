719cc15cdb620e019bbbd5f303884639031cc68e
rukshani <rukshani@node-33.largecluster.rotornet-pg0.utah.cloudlab.us>
2024-06-22 04:38:48 -0600

    analysis

--- z-analysis/ping_parser.py
+++ z-analysis/ping_parser.py
@@ -7,13 +7,13 @@ import pandas as pd
 
 
 ### File parsing
-path = "/tmp/slot-selection/direct-slot-100us.txt"
-path = "/tmp/slot-selection/direct-slot-200us.txt"
-path = "/tmp/slot-selection/direct-slot-1ms.txt"
+# path = "/tmp/slot-selection/direct-slot-100us.txt"
+# path = "/tmp/slot-selection/direct-slot-200us.txt"
+# path = "/tmp/slot-selection/direct-slot-1ms.txt"
 
-path = "/tmp/slot-selection/opera-slot-100us.txt"
-path = "/tmp/slot-selection/opera-slot-200us.txt"
-path = "/tmp/slot-selection/opera-slot-1ms.txt"
+# path = "/tmp/slot-selection/opera-slot-100us.txt"
+# path = "/tmp/slot-selection/opera-slot-200us.txt"
+# path = "/tmp/slot-selection/opera-slot-1ms.txt"
 
 lastColumn = [ ]
 
--- z-analysis/plot_ping.py
+++ z-analysis/plot_ping.py
@@ -4,6 +4,9 @@ import seaborn as sns
 import argparse
 import logging
 import pandas as pd
+import plotly.graph_objs as go
+import plotly
+import plotly.express as px
 
 def convert_to_us(value_something):
     return value_something * 1000
@@ -13,17 +16,17 @@ def read_file(path):
     rtt_df['rtt_us'] = rtt_df.rtt_ms.apply(convert_to_us)
     return rtt_df
 
-direct_100us = read_file("/tmp/slot-selection/direct-slot-100us-ms.csv")
+# direct_100us = read_file("/tmp/slot-selection/direct-slot-100us-ms.csv")
 direct_200us = read_file("/tmp/slot-selection/direct-slot-200us-ms.csv")
-direct_1ms = read_file("/tmp/slot-selection/direct-slot-1ms-ms.csv")
+# direct_1ms = read_file("/tmp/slot-selection/direct-slot-1ms-ms.csv")
 
 opera_100us = read_file("/tmp/slot-selection/opera-slot-100us-ms.csv")
 opera_200us = read_file("/tmp/slot-selection/opera-slot-200us-ms.csv")
 opera_1ms = read_file("/tmp/slot-selection/opera-slot-1ms-ms.csv")
 
-sns.kdeplot(data = direct_100us['rtt_us'], cumulative = True, label = "Direct-100us-slot")
+# sns.kdeplot(data = direct_100us['rtt_us'], cumulative = True, label = "Direct-100us-slot")
 sns.kdeplot(data = direct_200us['rtt_us'], cumulative = True, label = "Direct-200us-slot")
-sns.kdeplot(data = direct_1ms['rtt_us'], cumulative = True, label = "Direct-1ms-slot")
+# sns.kdeplot(data = direct_1ms['rtt_us'], cumulative = True, label = "Direct-1ms-slot")
 sns.kdeplot(data = opera_100us['rtt_us'], cumulative = True, label = "Opera-100us-slot")
 sns.kdeplot(data = opera_200us['rtt_us'], cumulative = True, label = "Opera-200us-slot")
 sns.kdeplot(data = opera_1ms['rtt_us'], cumulative = True, label = "Opera-1ms-slot")
@@ -33,4 +36,6 @@ plt.ylabel('CDF')
 
 # plt.xscale('log')
 
-plt.savefig('/tmp/slot-selection/all-slot-rtt.png')
+plt.savefig('all-slot.png')
+# fig = go.Figure()
+# fig.write_html("all-slot-rtt..html")
