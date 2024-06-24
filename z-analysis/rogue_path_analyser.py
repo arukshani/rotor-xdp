e099c07076d68fed8bdc6cace35ceabee8b915f3
rukshani <rukz.atha@gmail.com>
2024-06-24 09:56:11 -0600

    rogue path analysis

--- z-analysis/rogue_path_analyser.py
+++ z-analysis/rogue_path_analyser.py
@@ -13,31 +13,51 @@ import re
 import numpy as np
 import seaborn as sns
 
-summary_path = "/tmp/with_icmp_seq/"
+summary_path = "/tmp/paper-latency-hop/"
 
-
-n1_df = pd.read_csv(summary_path+"n1-topo-track-1ms.csv" ,sep=',', names=["n1_send_topo", "n1_recv_topo", 
+def read_file(n1_file_name, n2_file_name):
+    n1_df = pd.read_csv(summary_path+n1_file_name ,sep=',', names=["n1_send_topo", "n1_recv_topo", 
                                                         "ret_hop_count", "n1_send_seq", "n1_recv_seq"])
-n2_df = pd.read_csv(summary_path+"n2-topo-track-1ms.csv" ,sep=',', names=["n2_recv_topo", "n2_send_top", 
+    n2_df = pd.read_csv(summary_path+n2_file_name ,sep=',', names=["n2_recv_topo", "n2_send_topo", 
                                                         "fwd_hop_count", "n2_recv_seq", "n2_send_seq"])
+    
+    combined_df = pd.concat([n1_df, n2_df], axis=1)
+    # print(combined_df.head(5))
+    return combined_df
 
-print(n1_df.head(5))
-print(n2_df.head(5))
+def get_rogue_path_count(combined_df):
+    rogue_path_packet_count = 0
+    for i in range(0,50000):
+        row = combined_df.iloc[i]
+        if (row['n1_send_seq'] == row['n1_recv_seq'] == row['n2_recv_seq'] == row['n2_send_seq']):
+            if (row['n1_send_topo'] != row['n2_recv_topo']):
+                rogue_path_packet_count = rogue_path_packet_count + 1
+            if (row['n2_send_topo'] != row['n1_recv_topo']):
+                rogue_path_packet_count = rogue_path_packet_count + 1
+        else: 
+            print("Seq numbers don't match")
+    return rogue_path_packet_count
 
-# print(n1_df.count())
-# print(n2_df.count())
+combined_1ms_df = read_file("n1-topo-track-1ms.csv", "n2-topo-track-1ms.csv")
+# print(combined_1ms_df.count())
+# print(combined_1ms_df.head(5))
+rogue_count_1ms=get_rogue_path_count(combined_1ms_df)
+rogue_perc_1ms = (rogue_count_1ms/100000) * 100
+print(rogue_perc_1ms) #2.648
 
-rogue_path_packet_count = 0
+combined_100us_df = read_file("n1-topo-track-100us.csv", "n2-topo-track-100us.csv")
+# print(combined_1ms_df.count())
+# print(combined_1ms_df.head(5))
+rogue_count_100us=get_rogue_path_count(combined_100us_df)
+rogue_perc_100us = (rogue_count_100us/100000) * 100
+print(rogue_perc_100us) #29.469
 
-for i in range(0,30000):
-    n1_row=n1_df.iloc[i]
-    n2_row=n2_df.iloc[i]
-    if (n1_row['n1_send_seq'] == n1_row['n1_recv_seq'] == n2_row['n2_recv_seq'] == n2_row['n2_send_seq']):
-        if not (n1_row['n1_send_topo'] == n1_row['n1_recv_topo'] == n2_row['n2_recv_topo'] == n2_row['n2_send_top']):
-            rogue_path_packet_count = rogue_path_packet_count + 1
-    else: 
-        print("Seq numbers are not the same")
+combined_200us_df = read_file("n1-topo-track-200us.csv", "n2-topo-track-200us.csv")
+# print(combined_1ms_df.count())
+# print(combined_1ms_df.head(5))
+rogue_count_200us=get_rogue_path_count(combined_200us_df)
+rogue_perc_200us = (rogue_count_200us/100000) * 100
+print(rogue_perc_200us) #12.726
 
-print(rogue_path_packet_count)
 
 
