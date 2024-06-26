736b25a7ca26c24523df221ea495c5a142de8311
rukshani <rukz.atha@gmail.com>
2024-06-26 03:17:41 -0600

    analysis

---
+++ z-analysis/opera_retrans.py
@@ -0,0 +1,52 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import seaborn as sns
+import argparse
+import logging
+import pandas as pd
+
+
+def parse_log(file_name):
+    text_file = open(file_name, "r")
+    lines = []
+    retransmission_count ={}
+    for index, text in enumerate(text_file):
+        if 4 <= index <= 65:
+            lines.append(text)
+    for i in range(0,60):
+        # print(lines[i])
+        parts = lines[i].split() 
+        # print(parts[8]) 
+        retransmission_count[i] = int(parts[8])
+    return retransmission_count
+        
+
+retrans_1 = parse_log("data/opera_tcp_test1.txt")
+df_1 = pd.DataFrame(retrans_1.items(), columns=['time', 'retrans_count'])
+
+retrans_2 = parse_log("data/opera_tcp_test2.txt")
+df_2 = pd.DataFrame(retrans_2.items(), columns=['time', 'retrans_count'])
+
+retrans_3 = parse_log("data/opera_tcp_test3.txt")
+df_3 = pd.DataFrame(retrans_3.items(), columns=['time', 'retrans_count'])
+
+retrans_4 = parse_log("data/opera_tcp_test4.txt")
+df_4 = pd.DataFrame(retrans_4.items(), columns=['time', 'retrans_count'])
+
+retrans_5 = parse_log("data/opera_tcp_test5.txt")
+df_5 = pd.DataFrame(retrans_5.items(), columns=['time', 'retrans_count'])
+
+ax = df_1.plot(x="time", y=["retrans_count"],kind="line")
+df_2.plot(ax=ax, x="time", y=["retrans_count"],kind="line")
+df_3.plot(ax=ax, x="time", y=["retrans_count"],kind="line")
+df_4.plot(ax=ax, x="time", y=["retrans_count"],kind="line")
+df_5.plot(ax=ax, x="time", y=["retrans_count"],kind="line")
+
+plt.legend(["Trial 1", "Trial 2", "Trial 3", "Trial 4", "Trial 5"], fontsize=14)
+plt.xticks(fontsize=14)
+plt.yticks(fontsize=14)
+plt.xlabel('Time(s)', fontsize=14)
+plt.ylabel('Number of Retransmissions', fontsize=14)
+# plt.savefig('P-Opera-retrans-count.png')
+plt.savefig('P-Opera-retrans-count.pdf')
+
