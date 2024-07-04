bc9c6fa3b241a8003d53ab9ff9ef01cd05ae0c75
rukshani <rukz.atha@gmail.com>
2024-07-03 23:25:57 -0600

    analysis

--- z-analysis/single_seq_analysis.py
+++ z-analysis/single_seq_analysis.py
@@ -14,51 +14,39 @@ import numpy as np
 import seaborn as sns
 
 path = "data/seq/"
+filename = "2-direct-node-1.csv"
+plotname = 'data/seq/plots/2-direct-topo.png'
 
 def read_file(n1_file_name):
     n1_df = pd.read_csv(path+n1_file_name ,sep=',')
     return n1_df
 
-n1_df = read_file("1-direct-node-1.csv")
+n1_df = read_file(filename)
 n1_df = n1_df.loc[(n1_df['slot'] == 0)]
-n1_df = n1_df.loc[(n1_df['is_fin'] != 1)]
-# n1_df = n1_df.loc[(n1_df['ns_packet_len'] == 3414)]
-# print(n1_df.head(15))
+# n1_df = n1_df.loc[(n1_df['src_port'] == 52202)]
 
 pos = n1_df.columns.get_loc('time_ns')
 n1_df['elaps_time_ns'] =  n1_df.iloc[1:, pos] - n1_df.iat[0, pos]
 n1_df['elaps_time_us'] =  n1_df['elaps_time_ns'] /1000
 n1_df.replace(np.nan, 0, inplace=True)
-n1_df = n1_df.head(1200) #0-1200
-# n1_df = n1_df[2640:3500] 
-# print(n1_df.head(20))
-# n1_df=n1_df[n1_df.index.isin([1201,2400])]
-# print(n1_df.size)
+# n1_df = n1_df.head(1000) #0-1200
+n1_df = n1_df[5400:5600] 
 
-# print(n1_df[0:15])
-
-# index_list = [0,10]
-# test = n1_df.iloc[0,10]
-# print(test)
-# n1_df = n1_df.loc[n1_df.index[index_list]]
 # print(n1_df[['seq', 'topo_arr', 'elaps_time_us']])
 
 periods = n1_df[n1_df.topo_arr.diff()!=0].elaps_time_us
-
 labels = n1_df[n1_df.topo_arr.diff()!=0].topo_arr
-
 indices = n1_df[n1_df.topo_arr.diff()!=0].index.values
-# print(indices)
 
 plt.plot(n1_df['elaps_time_us'], n1_df['seq'], label = "direct", marker='|')
-# for item in periods:
-#     plt.axvline(item, ymin=0, ymax=1,color='red')
+for item in periods:
+    plt.axvline(item, ymin=0, ymax=1,color='red')
 
-# for i in range(len(indices)-1):
-#     index = indices[i]
-#     next_index = indices[i+1]
-#     plt.text(y=3102196394,x=(periods[index]),
-#         s=str(labels[index]), color='black', fontsize=7)
+for i in range(len(indices)):
+    index = indices[i]
+    # next_index = indices[i+1]
+    plt.text(y=n1_df['seq'].max(),x=(periods[index]),
+        s=str(labels[index]), color='black', fontsize=10)
 
 # ax = sns.lineplot(x="elaps_time_us", y="seq", data=n1_df)
 # ax.axvspan(3, 100,facecolor="darkmagenta", edgecolor='black', hatch="o", alpha=.3, label="Easter week")
@@ -68,6 +56,10 @@ plt.xticks(fontsize=11)
 plt.yticks(fontsize=11)
 plt.xlabel('time (us)', fontsize=11)
 plt.ylabel('sequence number', fontsize=11)
-plt.savefig('data/seq/1-seq-direct-all.png')
+plt.savefig(plotname)
 # plt.savefig('P-ALL-RTTs.pdf')
+
+# fig = go.Figure()
+# fig.add_traces(go.Scatter(x=n1_df['elaps_time_us'], y = n1_df['seq'], name="direct", mode='markers'))
+# fig.write_html("data/seq/2-seq-direct-all.html")
 
