a4cea170df55470751f7aa930d74c741605c89dc
rukshani <rukz.atha@gmail.com>
2024-07-05 02:20:25 -0600

    cwnd analyser

--- z-analysis/cwnd_analyser.py
+++ z-analysis/cwnd_analyser.py
@@ -23,9 +23,12 @@ trace_file = "4-cwnd-direct-node-1.csv"
 plotname = "4-cwnd-direct-node-1.png"
 
 trace_df = pd.read_csv(path+trace_file ,sep=',')
+pos = trace_df.columns.get_loc('time')
+trace_df['elaps_time'] =  trace_df.iloc[1:, pos] - trace_df.iat[0, pos]
+trace_df['elaps_time_us'] =  trace_df['elaps_time'] * 1000000
 print(trace_df.head(5))
 
-plt.plot(trace_df['time'], trace_df['srtt'], label = "srtt")
+plt.plot(trace_df['elaps_time_us'], trace_df['snd_cwnd'], label = "snd_cwnd", marker="o")
 
 # fig, ax = plt.subplots()
 # sns.ecdfplot(data=trace_df, x="snd_cwnd", ax=ax, label = "cwnd")
@@ -33,9 +36,9 @@ plt.plot(trace_df['time'], trace_df['srtt'], label = "srtt")
 plt.legend(fontsize=14)
 plt.xticks(fontsize=14)
 plt.yticks(fontsize=14)
-# plt.xlabel('time', fontsize=14)
-# plt.ylabel('srtt', fontsize=14)
-plt.savefig('srtt_no-ratelimit.png')
+plt.xlabel('time (us)', fontsize=14)
+plt.ylabel('cwnd (packets)', fontsize=14)
+plt.savefig(plot_path+plotname)
 # plt.savefig('P-ALL-RTTs.pdf')
 
  
