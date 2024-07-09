cd06d071452006b4b5aa287a2acbde9daa26353c
rukshani <rukz.atha@gmail.com>
2024-07-09 01:13:17 -0600

    increase stack size

--- z-test-tools/node_health.py
+++ z-test-tools/node_health.py
@@ -5,6 +5,14 @@ import pickle
 import logging
 import pandas as pd
 
+def increase_stack_size():
+    with open('/tmp/workers.pkl','rb') as f:  
+        workers = pickle.load(f)
+        for worker in workers:
+            print("===================READ start log==={}=======================".format(worker['host']))
+            remoteCmd = 'ssh -o StrictHostKeyChecking=no {}@{} "bash -s" < ./slack_size.sh'.format(worker['username'],worker['host'])
+            proc = subprocess.run(remoteCmd, shell=True)
+
 def emu_process_check():
     with open('/tmp/workers.pkl','rb') as f:  
         workers = pickle.load(f)
@@ -37,12 +45,15 @@ def main(args):
         delete_logs()
     if(args.check):
         emu_process_check()
+    if(args.stack):
+        increase_stack_size()
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Start and stop PTP on worker nodes')
     parser.add_argument('--read', '-r', action='store_true')
     parser.add_argument('--delete', '-d', action='store_true')
-    parser.add_argument('--check', '-c', action='store_true')
+    parser.add_argument('--process', '-p', action='store_true')
+    parser.add_argument('--stack', '-s', action='store_true')
     args = parser.parse_args()
     return args
     
