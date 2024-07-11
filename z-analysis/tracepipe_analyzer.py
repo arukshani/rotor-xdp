ef38322add676475cd68540723d5717e98879b55
rukshani <rukz.atha@gmail.com>
2024-07-11 06:59:30 -0600

    analysis

--- z-analysis/tracepipe_analyzer.py
+++ z-analysis/tracepipe_analyzer.py
@@ -16,15 +16,15 @@ import seaborn as sns
 def get_tot(fwd, ret):
     return (fwd+ret)
 
-path = "throughput/direct-2/"
-trace_file = "2-direct-cwnd-node-1.csv"
-plotname = "throughput/direct-2/plots/direct-2-cwnd.png"
+path = "throughput/iperf3/"
+trace_file = "1-opera-cwnd-node-1.csv"
+plotname = "throughput/iperf3/1-opera-iperf-srtt.png"
 
 trace_df = pd.read_csv(path+trace_file ,sep=',')
 # print(trace_df.head(5))
 
-# plt.plot(trace_df['time'], trace_df['srtt'], label = "srtt")
-plt.plot(trace_df['time'], trace_df['snd_cwnd'], label = "snd_cwnd")
+plt.plot(trace_df['time'], trace_df['srtt'], label = "srtt")
+# plt.plot(trace_df['time'], trace_df['snd_cwnd'], label = "snd_cwnd")
 
 # fig, ax = plt.subplots()
 # sns.ecdfplot(data=trace_df, x="snd_cwnd", ax=ax, label = "cwnd")
@@ -33,8 +33,8 @@ plt.legend(fontsize=11)
 plt.xticks(fontsize=11)
 plt.yticks(fontsize=11)
 plt.xlabel('time', fontsize=11)
-plt.ylabel('cwnd (packets)', fontsize=11)
-# plt.ylabel('srtt (us)', fontsize=11)
+# plt.ylabel('cwnd (packets)', fontsize=11)
+plt.ylabel('srtt (us)', fontsize=11)
 plt.savefig(plotname)
 # plt.savefig('P-ALL-RTTs.pdf')
 
--- z-analysis/tracepipe_parser.py
+++ z-analysis/tracepipe_parser.py
@@ -8,9 +8,9 @@ import fileinput
 
 # print("hello")
 
-path = "throughput/direct-2/"
-read_file_name = path+"2-direct-cwnd-node-1.txt"
-write_file_name = path+"2-direct-cwnd-node-1.csv"
+path = "throughput/iperf3/"
+read_file_name = path+"1-direct-cwnd-node-1.txt"
+write_file_name = path+"1-direct-cwnd-node-1.csv"
 
 with open(write_file_name, 'a') as f:
     f.write("{},{},{},{},{},{},{},{},{},{},{}\n".format('time','src','dest','data_len','snd_nxt','snd_una','snd_cwnd','ssthresh',
@@ -20,34 +20,36 @@ with open(write_file_name, 'a') as f:
         # if ('iperf3' in line):
         # if ('emu_nic' in line):
             # print(line)
+           
             line_parts = line.split(' ')
 
-            time = line_parts[7][:-1]
-            src = line_parts[10].split('=')[1]
-            dest = line_parts[11].split('=')[1]
-            data_len = line_parts[13].split('=')[1]
-            snd_nxt = line_parts[14].split('=')[1]
-            snd_una = line_parts[15].split('=')[1]
-            snd_cwnd = line_parts[16].split('=')[1]
-            ssthresh = line_parts[17].split('=')[1]
-            snd_wnd = line_parts[18].split('=')[1]
-            srtt = line_parts[19].split('=')[1]
-            rcv_wnd = line_parts[20].split('=')[1]
+            # time = line_parts[7][:-1]
+            # src = line_parts[10].split('=')[1]
+            # dest = line_parts[11].split('=')[1]
+            # data_len = line_parts[13].split('=')[1]
+            # snd_nxt = line_parts[14].split('=')[1]
+            # snd_una = line_parts[15].split('=')[1]
+            # snd_cwnd = line_parts[16].split('=')[1]
+            # ssthresh = line_parts[17].split('=')[1]
+            # snd_wnd = line_parts[18].split('=')[1]
+            # srtt = line_parts[19].split('=')[1]
+            # rcv_wnd = line_parts[20].split('=')[1]
             # print(srtt)
 
-            # time = line_parts[15][:-1]
-            # src = line_parts[18].split('=')[1]
-            # dest = line_parts[19].split('=')[1]
-            # data_len = line_parts[21].split('=')[1]
-            # snd_nxt = line_parts[22].split('=')[1]
-            # snd_una = line_parts[23].split('=')[1]
-            # snd_cwnd = line_parts[24].split('=')[1]
-            # ssthresh = line_parts[25].split('=')[1]
-            # snd_wnd = line_parts[26].split('=')[1]
-            # srtt = line_parts[27].split('=')[1]
-            # rcv_wnd = line_parts[28].split('=')[1]
+            time = line_parts[14][:-1]
+            src = line_parts[17].split('=')[1]
+            dest = line_parts[18].split('=')[1]
+            data_len = line_parts[20].split('=')[1]
+            snd_nxt = line_parts[21].split('=')[1]
+            snd_una = line_parts[22].split('=')[1]
+            snd_cwnd = line_parts[23].split('=')[1]
+            ssthresh = line_parts[24].split('=')[1]
+            snd_wnd = line_parts[25].split('=')[1]
+            srtt = line_parts[26].split('=')[1]
+            rcv_wnd = line_parts[27].split('=')[1]
             # print ("{},{},{},{},{},{},{},{},{},{},{}\n".format(time,src,dest,data_len,snd_nxt,snd_una,snd_cwnd,ssthresh,
             #                     snd_wnd,srtt,rcv_wnd))
+            # break
             f.write("{},{},{},{},{},{},{},{},{},{},{}\n".format(time,src,dest,data_len,snd_nxt,snd_una,snd_cwnd,ssthresh,
                                 snd_wnd,srtt,rcv_wnd))
 f.close()
