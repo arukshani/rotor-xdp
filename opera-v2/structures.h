12aceefb0ae0145a9fef366c0493d37386486cab
rukshani <rukz.atha@gmail.com>
2024-07-14 04:00:23 -0600

    q size

--- opera-v2/structures.h
+++ opera-v2/structures.h
@@ -30,7 +30,7 @@ typedef __u8  u8;
 
 #ifndef MAX_BURST_TX_OBJS
 // #define MAX_BURST_TX_OBJS 4096
-#define MAX_BURST_TX_OBJS 6000
+#define MAX_BURST_TX_OBJS 4500
 #endif
 
 #ifndef NUM_OF_PER_DEST_QUEUES
