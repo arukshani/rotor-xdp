66fc2d9e10ae2de00d23579790785934ed78d5be
rukshani <rukshani@node-33.testbed2.rotornet-pg0.utah.cloudlab.us>
2024-07-24 03:39:49 -0600

    test

--- emulator-setup/change_iface_properties.sh
+++ emulator-setup/change_iface_properties.sh
@@ -6,8 +6,8 @@ NODE_INTERFACE=${NODE_INTERFACE::-1}
 sudo ip link set $NODE_INTERFACE mtu 3490
 sudo ethtool -L $NODE_INTERFACE combined 1
 
-sudo ethtool -G $NODE_INTERFACE rx 2048
-sudo ethtool -G $NODE_INTERFACE tx 2048
+# sudo ethtool -G $NODE_INTERFACE rx 2048
+# sudo ethtool -G $NODE_INTERFACE tx 2048
 
-cd /opt/rotor-xdp/emulator-setup/
-sudo ./set_irq_affinity.sh $NODE_INTERFACE
+# cd /opt/rotor-xdp/emulator-setup/
+# sudo ./set_irq_affinity.sh $NODE_INTERFACE
